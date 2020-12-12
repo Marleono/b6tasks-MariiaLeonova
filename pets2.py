a0cf6c92d00216dd44ecc0642bd12a5aeeac0cc3
Marleono <73365729+Marleono@users.noreply.github.com>
2020-12-12 20:39:54 +0300

    Add files via upload

---
+++ pets2.py
@@ -0,0 +1,11 @@
+from pets import Cats
+
+cat1 = Cats('Барон','мальчик',2)
+cat2 = Cats('Сэм','мальчик',2)
+
+print(cat1.name)
+print(cat1.sex)
+print(cat1.age)
+print(cat2.name)
+print(cat2.sex)
+print(cat2.age)
